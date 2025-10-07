a05d7dcc9bc3ae4556662174ff9df039ab093ce3
Meh <115894849+notMehrzad@users.noreply.github.com>
2025-10-08 01:54:53 +0330

    starter bot, updating requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("the bot is ready")
+import discord
+import json
+
+with open("config.json") as file:
+    config = json.load(file)
+
+prefix = "!"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user} ✅')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(f"{prefix}hello"):
+        await message.channel.send("Hello!")
+
+client.run(config["TOKEN"])
