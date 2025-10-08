93db5862eb25085c1bb603b26b1e0e8925858225
Meh <115894849+notMehrzad@users.noreply.github.com>
2025-10-09 00:41:28 +0330

    improved ping command, unfinished embed command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import time
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='.', intents=intents)
@@ -10,10 +11,41 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user} ✅')
 
-#registering the ping command
+
+#ping command
 @bot.command(name = "ping")
 async def ping(ctx):
-    await ctx.reply("Pong!")
+    pings = []
+    pingNumbers = 4
+
+    msg = await ctx.reply("pinging...")
+
+    ws = bot.latency * 1000
+
+    #starts pinging n times
+    for i in range(pingNumbers):
+        start = time.perf_counter()
+        await msg.edit(content = f"ping {i + 1}..")
+        end = time.perf_counter()
+
+        msg_latency = (end - start) * 1000
+        pings.append(msg_latency) #stores the nth ping in a list
+
+    avg = sum(pings) / pingNumbers
+    min = min(pings)
+    max = max(pings)
+    await msg.edit(content = (
+        "Pong! 🏓\n"
+        f"📡WebSocket Latency: `{ws:.2f} ms`\n"
+        f"🌐REST Latency: `{avg:.2f} ms`\n"
+        f"Min: `{min:.2f} ms` | Max: `{max:.2f} ms`"
+    ))
+
+
+#embed message command
+@bot.command(name = "embed")
+async def embed(ctx, *, args):
+    pass
 
 
 #reading the stored token from config.json
