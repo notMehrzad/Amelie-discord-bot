ff473ab0681055e64a1cd4557e11ab86d375e938
Meh <115894849+notMehrzad@users.noreply.github.com>
2025-10-08 13:10:41 +0330

    a few fixes with api, added the ping command

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import discord
+from discord.ext import commands
 import json
 
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+#print a message when bot is ready
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user} ✅')
+
+#registering the ping command
+@bot.command(name = "ping")
+async def ping(ctx):
+    await ctx.reply("Pong!")
+
+
+#reading the stored token from config.json
 with open("config.json") as file:
     config = json.load(file)
 
-prefix = "!"
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user} ✅')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith(f"{prefix}hello"):
-        await message.channel.send("Hello!")
-
-client.run(config["TOKEN"])
+#runs the bot
+bot.run(config["TOKEN"])
