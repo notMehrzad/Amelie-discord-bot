730ff81b0244ede86dfb40dcb6ffe8bcf3a95e9c
Meh <115894849+notMehrzad@users.noreply.github.com>
2025-11-01 18:55:19 +0330

    finished mod cog

--- cogs/mod.py
+++ cogs/mod.py
@@ -17,142 +17,245 @@ def timeDeltaParser(untilStr: str):
 class Mod(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
-    @commands.command(name = "mod")
-    async def mod(self, ctx: commands.Context[commands.Bot], cmd: str | None = None, target: discord.Member | None = None, untilStr: str | None = None, *, reason: str | None = None):
-        print(cmd, target)
+        
+    #kick subcommand
+    @commands.command(name = "kick", aliases = ["k"])
+    async def kick(self, ctx: commands.Context[commands.Bot], target: discord.Member | None = None, *, reason: str = "`no reason provided`"):
         #if user runs the command in dm
         if not ctx.guild or not isinstance(ctx.author, discord.Member):
             return await ctx.reply("You can only do moderation commands in a server.")
         
-        #if user dosn't enter any subcommand
-        if not cmd:
-            return await ctx.reply("You must enter a subcommand for this command.")
+        #if the user has no permission to kick
+        if not ctx.author.guild_permissions.kick_members:
+            return await ctx.reply("You have no permission to *kick* Members.")
+        
+        #if the bot has no permission to kick
+        if not ctx.guild.me.guild_permissions.kick_members:
+            return await ctx.reply("I have no permisson to *kick* Members.")
         
         #if user didn't enter any target member
         if not target:
             return await ctx.reply("You must enter a target user for this command.")
         
-        cmd = cmd.lower()
-            
-        #if user enters an invalid subcommand
-        if cmd not in ["kick", "k", "ban", "b", "timeout", "t", "unban", "ub"]:
-            return await ctx.reply("You must enter a valid subcommand for this command.")
-        
-        #if user wants to do moderation command on himself
+        #if user wants to kick himself
         if target.id == ctx.author.id:
-            return await ctx.reply("You can't do moderation commands on yourself.")
+            return await ctx.reply("You can't kick yourself!")
         
         #if user wants to do moderation command on the bot
         if target.id == ctx.me.id:
             return await ctx.reply("You can't do my moderation commands on myself.\nnice try.")
         
-        #if user trys to kick the owner
+        #if user trys to kick the server owner
         if target.id == ctx.guild.owner_id:
-            return await ctx.reply("You can't do moderation commands on the *Owner*.")
+            return await ctx.reply("You can't kick the *Owner*.")
         
-        #kick subcommand
-        if cmd in ["kick", "k"]:
-            #if the user has no permission to kick
-            if not ctx.author.guild_permissions.kick_members:
-                return await ctx.reply("You have no permission to *kick* Members.")
-            
-            #if the bot has no permission to kick
-            if not ctx.guild.me.guild_permissions.kick_members:
-                return await ctx.reply("I have no permisson to *kick* Members.")
-            
-            #if user has lower or equal role position than target
-            if target.top_role >= ctx.author.top_role and ctx.author.id != ctx.guild.owner_id:
-                return await ctx.reply(f"You can't kick a Member with *higher or equal* role position as you.")
-            
-            #if the bot has lower or equal role position than target
-            if target.top_role >= ctx.guild.me.top_role:
-                return await ctx.reply(f"I can't kick a Member with *higher or equal* role position as me.")
-            
-            #kicks the target
-            try:
-                await target.kick(reason = reason)
-            except discord.Forbidden:
-                await ctx.reply("I haven't the proper permission to kick Members.")
+        #if user has lower or equal role position than target
+        if target.top_role >= ctx.author.top_role and ctx.author.id != ctx.guild.owner_id:
+            return await ctx.reply("You can't kick a Member with *higher or equal* role position as you.")
+        
+        #if the bot has lower or equal role position than target
+        if target.top_role >= ctx.guild.me.top_role:
+            return await ctx.reply("I can't kick a Member with *higher or equal* role position as me.")
+        
+        #kicks the target
+        try:
+            await target.kick(reason = reason)
+            await ctx.reply(f"{target.display_name} has been *kicked* via {ctx.author.display_name}.\nreason: {reason}")
+        except discord.Forbidden:
+            await ctx.reply("I haven't the proper permission to kick Members.")
 
-        #ban subcommand
-        elif cmd in ["ban", "b"]:
-            #if the user has no permission to ban
-            if not ctx.author.guild_permissions.ban_members:
-                return await ctx.reply("You have no permission to *ban* Members.")
-            
-            #if the bot has no permission to ban
-            if not ctx.guild.me.guild_permissions.ban_members:
-                return await ctx.reply("I have no permisson to *ban* Members.")
-            
-            #if user has lower or equal role position than target
-            if target.top_role >= ctx.author.top_role and ctx.author.id != ctx.guild.owner_id:
-                return await ctx.reply(f"You can't ban a Member with *higher or equal* role position as you.")
-            
-            #if the bot has lower or equal role position than target
-            if target.top_role >= ctx.guild.me.top_role:
-                return await ctx.reply(f"I can't ban a Member with *higher or equal* role position as me.")
-            
-            #bans the target
-            try:
-                await target.ban(reason = reason)
-            except discord.Forbidden:
-                await ctx.reply("I haven't the proper permission to ban Members.")
+    @kick.error
+    async def kick_error(self, ctx: commands.Context[commands.Bot], error: commands.CommandError):
+        #if user entered an invalid user
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("Member not found. Please enter a valid user.")
+        else:
+            print(f"❌ something went wrong with mod-kick command: {error}")
+            await ctx.reply("something went wrong with **kick**.")
 
-        #time out subcommand
-        elif cmd in ["timeout", "t"]:
-            #if the user has no permission to time out
-            if not ctx.author.guild_permissions.moderate_members:
-                return await ctx.reply("You have no permission to *time out* Members.")
-            
-            #if the bot has no permission to time out
-            if not ctx.guild.me.guild_permissions.moderate_members:
-                return await ctx.reply("I have no permisson to *time out* Members.")
-            
-            #if user has lower or equal role position than target
-            if target.top_role >= ctx.author.top_role and ctx.author.id != ctx.guild.owner_id:
-                return await ctx.reply(f"You can't time out a Member with *higher or equal* role position as you.")
-            
-            #if the bot has lower or equal role position than target
-            if target.top_role >= ctx.guild.me.top_role:
-                return await ctx.reply(f"I can't time out a Member with *higher or equal* role position as me.")
-            
-            if not untilStr:
-                return await ctx.reply("You must specify a time so this Member will be timed out until then. or false/0 to remove the timeout.")
-            
-            #time outs the target
-            try:
-                until = dateparser.parse(untilStr) #parses if user enters datetime, None if invalid
-
-                if not until:
-                    until = timeDeltaParser(untilStr) #parses if user enters timedelta and it wasn't datetime, None if invalid
-
-                #if entered until wasn't either a datetime or timedelta
-                if not until:
-                    if untilStr.lower() in ["false", "0", "remove"]:
-                        await target.timeout(None, reason = reason)
-                        await ctx.reply(f"{target.display_name}'s time out has been *removed* via {ctx.author.display_name}.")
-                        return
-                    return await ctx.reply("enter a valid datetime.")
-                
-                await target.timeout(until, reason = reason)
-                await ctx.reply(f"{target.display_name} has been timed out via {ctx.author.display_name}" + f" for `{until}` ." if isinstance(until, timedelta) else f" until `{until}` .")
-
-            except TypeError:
-                await ctx.reply("enter a valid datetime.")
-
-        elif cmd in ["unban", "ub"]:
+    #ban subcommand
+    @commands.command(name = "ban", aliases = ["b"])
+    async def ban(self, ctx: commands.Context[commands.Bot], target: discord.Member | None = None, *, reason: str | None = None):
+        #if user runs the command in dm
+        if not ctx.guild or not isinstance(ctx.author, discord.Member):
+            return await ctx.reply("You can only do moderation commands in a server.")
+        
+        #if the user has no permission to ban
+        if not ctx.author.guild_permissions.ban_members:
+            return await ctx.reply("You have no permission to *ban* Members.")
+        
+        #if the bot has no permission to ban
+        if not ctx.guild.me.guild_permissions.ban_members:
+            return await ctx.reply("I have no permisson to *ban* Members.")
+        
+        #if user didn't enter any target member
+        if not target:
+            return await ctx.reply("You must enter a target user for this command.")
+        
+        #if user wants to ban himself
+        if target.id == ctx.author.id:
+            return await ctx.reply("You can't ban yourself!")
+        
+        #if user wants to do moderation command on the bot
+        if target.id == ctx.me.id:
+            return await ctx.reply("You can't do my moderation commands on myself.\nnice try.")
+        
+        #if user trys to ban the server owner
+        if target.id == ctx.guild.owner_id:
+            return await ctx.reply("You can't ban the *Owner*.")
+        
+        #if user has lower or equal role position than target
+        if target.top_role >= ctx.author.top_role and ctx.author.id != ctx.guild.owner_id:
+            return await ctx.reply("You can't ban a Member with *higher or equal* role position as you.")
+        
+        #if the bot has lower or equal role position than target
+        if target.top_role >= ctx.guild.me.top_role:
+            return await ctx.reply("I can't ban a Member with *higher or equal* role position as me.")
+        
+        #checks if user is banned already
+        try:
+            await ctx.guild.fetch_ban(discord.Object(id = target.id))
+            await ctx.reply(f"{target.display_name} is banned already.")
+            return
+        except discord.NotFound:
             pass
         
-    @mod.error
-    async def mod_error(self, ctx: commands.Context[commands.Bot], error: commands.CommandError):
-        #if user mentioned an invalid user
+        #bans the target
+        try:
+            await ctx.guild.ban(user = target, reason = reason)
+            await ctx.reply(f"{target.display_name} has been *banned* via {ctx.author.display_name}.\nreason: {reason}")
+        except discord.Forbidden:
+            await ctx.reply("I haven't the proper permission to ban Members.")
+
+    @ban.error
+    async def ban_error(self, ctx: commands.Context[commands.Bot], error: commands.CommandError):
+        #if user entered an invalid user
         if isinstance(error, commands.BadArgument):
-            await ctx.reply("Member not found. Please mention a valid user.")
+            await ctx.reply("Member not found. Please enter a valid user.")
         else:
-            print(f"❌ something went wrong with mod command: {error}")
-            await ctx.reply("something went wrong with **mod**.")   
+            print(f"❌ something went wrong with mod-ban command: {error}")
+            await ctx.reply("something went wrong with **ban**.")
+
+    #timeout command
+    @commands.command(name = "timeout", aliases = ["to"])
+    async def timeout(self, ctx: commands.Context[commands.Bot], target: discord.Member | None = None, untilStr: str | None = None, *, reason: str = "`no reason provided`"):
+        #if user runs the command in dm
+        if not ctx.guild or not isinstance(ctx.author, discord.Member):
+            return await ctx.reply("You can only do moderation commands in a server.")
+        
+        #if the user has no permission to time out
+        if not ctx.author.guild_permissions.moderate_members:
+            return await ctx.reply("You have no permission to *time out* Members.")
+        
+        #if the bot has no permission to time out
+        if not ctx.guild.me.guild_permissions.moderate_members:
+            return await ctx.reply("I have no permisson to *time out* Members.")
+        
+        #if user didn't enter any target member
+        if not target:
+            return await ctx.reply("You must enter a target user for this command.")
+        
+        #if user trys to time out the server owner
+        if target.id == ctx.guild.owner_id:
+            return await ctx.reply("You can't timeout the server *owner*.")
+        
+        #if user has lower or equal role position than target
+        if target.top_role >= ctx.author.top_role and ctx.author.id != ctx.guild.owner_id:
+            return await ctx.reply("You can't time out a Member with *higher or equal* role position as you.")
+        
+        #if the bot has lower or equal role position than target
+        if target.top_role >= ctx.guild.me.top_role:
+            return await ctx.reply("I can't time out a Member with *higher or equal* role position as me.")
+        
+        if not untilStr:
+            return await ctx.reply("You must specify a time so this Member will be timed out until then. or false/0 to remove the timeout.")
+        
+        #time outs the target
+        try:
+            until = dateparser.parse(untilStr) or timeDeltaParser(untilStr)
+
+            #if entered until wasn't either a datetime or timedelta
+            if not until:
+                if untilStr.lower() in ["false", "0", "remove"]:
+                    await target.timeout(None, reason = reason)
+                    await ctx.reply(f"{target.display_name}'s time out has been *removed* via {ctx.author.display_name}.")
+                    return
+                else:
+                    return await ctx.reply("enter a valid datetime.")
+                
+            if isinstance(until, timedelta) and not (timedelta(days = 28) >= until >= timedelta(minutes = 2)):
+                return await ctx.reply("timeout must be at least *2 minutes.*")
+            
+            if target.is_timed_out():
+                return await ctx.reply(f"{target.display_name} is timed out already.")
+            
+            await target.timeout(until, reason = reason)
+
+            if isinstance(until, timedelta):
+                await ctx.reply(f"{target.display_name} has been timed out via {ctx.author.display_name} for `{until}` .\nreason: {reason}")
+
+            else:
+                await ctx.reply(f"{target.display_name} has been timed out via {ctx.author.display_name} until `{until}` .\nreason: {reason}")
+
+        except TypeError:
+            await ctx.reply("enter a valid datetime.")
+        
+    @timeout.error
+    async def timeout_error(self, ctx: commands.Context[commands.Bot], error: commands.CommandError):
+        #if user entered an invalid user
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("Member not found. Please enter a valid user.")
+        else:
+            print(f"❌ something went wrong with mod-timeout command: {error}")
+            await ctx.reply("something went wrong with **timeout**.")
+
+    @commands.command(name = "unban", aliases = ["ub"])
+    async def unban(self, ctx: commands.Context[commands.Bot], targetId: int | None = None, *, reason: str = "`no reason provided`"):
+        #if user runs the command in dm
+        if not ctx.guild or not isinstance(ctx.author, discord.Member):
+            return await ctx.reply("You can only do moderation commands in a server.")
+        
+        #if the user has no permission to unban
+        if not ctx.author.guild_permissions.ban_members:
+            return await ctx.reply("You have no permission to *unban* Members.")
+        
+        #if the bot has no permission to unban
+        if not ctx.guild.me.guild_permissions.ban_members:
+            return await ctx.reply("I have no permisson to *unban* Members.")
+        
+        #if user didn't enter any target member
+        if not targetId:
+            return await ctx.reply("You must enter a target user for this command.")
+        
+        #if user wants to unban himself
+        if targetId == ctx.author.id:
+            return await ctx.reply("You were never banned for you to undo it now.")
+        
+        #if user wants to do moderation command on the bot
+        if targetId == ctx.me.id:
+            return await ctx.reply("I was never (and can't be) banned for you to undo it now.")
+        
+        #if user trys to kick the server owner
+        if targetId == ctx.guild.owner_id:
+            return await ctx.reply("The server *Owner* was never (and couldn't be) banned for you to undo it now.")
+        
+        #unbans the target
+        target = await self.bot.fetch_user(targetId) #fetches the user from id
+        try:
+            entry = await ctx.guild.fetch_ban(discord.Object(id = targetId)) #checks if the target is banned
+            await ctx.guild.unban(user = entry.user, reason = reason)
+            await ctx.reply(f"{target.display_name} has been unbanned via {ctx.author.display_name}.\nreason: {reason}")
+        except discord.NotFound:
+            return await ctx.reply(f"{target.display_name} was never banned for you to undo it now.")
     
+    @unban.error
+    async def unban_error(self, ctx: commands.Context[commands.Bot], error: commands.CommandError):
+        #if user entered an invalid user
+        if isinstance(error, commands.BadArgument):
+            await ctx.reply("Member not found. Please enter a valid user.")
+        else:
+            print(f"❌ something went wrong with mod-unban command: {error}")
+            await ctx.reply("something went wrong with **unban**.")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Mod(bot))
