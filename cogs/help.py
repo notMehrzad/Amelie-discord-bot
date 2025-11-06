431122adc99703de02bdf977cc2d5d08c09e287c
Meh <115894849+notMehrzad@users.noreply.github.com>
2025-11-07 02:49:15 +0330

    help command changes

--- cogs/help.py
+++ cogs/help.py
@@ -7,36 +7,44 @@ class Help(commands.Cog):
 
     @commands.command(
             name = "help",
+            aliases = ["h"],
             help = (
-                "Shows a brief information about all commands such as their application, usage, necessery permissions (if any) and etc."
-                "Enter commands name for getting the full information about that command or nothing to get the list of brief overview."
+                "Shows the help menu which gives a brief information about Commands."
+                "\nEnter a Command name for its full detail such as its application, usage, necessery permissions (if any) and etc. or don't, to get the help menu."
             ),
-            brief = "Shows a brief information about all commands.",
-            usage = "<command_name[optional]>",
-            extras = {"category": "General"}
-
+            brief = "Shows the help menu.",
+            usage = f"<command_name*[optional]*>",
+            extras = {"Category": "General"}
     )
     async def help(self, ctx: commands.Context[commands.Bot], cmdStr: str | None = None):
         #help for specific command
-        if cmdStr:
-            cmd = self.bot.get_command(cmdStr)
+        if cmdStr and cmdStr.lower() not in ["all", "list", "menu"]:
+            cmd = self.bot.get_command(cmdStr.lower()) #fetches the command, None if not found
 
+            #if user doesn't enter a valid command name
             if not cmd:
-                return await ctx.reply(f"{cmdStr} doesn't exist. enter a valid command.")
+                return await ctx.reply(f"*{cmdStr}* doesn't exist. enter a valid command.")
             
             cmdEmbed = discord.Embed(
                 title = f"Help: {cmd.name}",
                 description = cmd.help or cmd.brief or "*no description*",
                 color = discord.Color.blurple()
             )
+            #if command has aliases
+            if cmd.aliases:
+                cmdEmbed.add_field(name = "Aliases", value = ", ".join(cmd.aliases))
+            
+            if cmd.usage:
+                cmdEmbed.add_field(name = "Usage", value = cmd.name + " " + cmd.usage)
+            #if command has extra information
             for key, value in cmd.extras.items():
-                if key == "category":
+                if key == "Category":
                     continue
-                cmdEmbed.add_field(name = key, value = value)
+                cmdEmbed.add_field(name = key, value = value) #add extra information to fields
 
             await ctx.reply(embed = cmdEmbed)
         
-        #shows a list of all commands
+        #shows the help menu
         else:
             pass
 
