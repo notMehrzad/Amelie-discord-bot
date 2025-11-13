848a8e5746b0cbcf164dccd18528e99fa545e275
Meh <115894849+notMehrzad@users.noreply.github.com>
2025-11-14 02:41:13 +0330

    initial sql commit\initial connection and setup

---
+++ cogs/database.py
@@ -0,0 +1,22 @@
+import aiosqlite
+
+#connection helper function
+async def connection():
+    connection = await aiosqlite.connect("bot_database.db") #connects to the db file
+    await connection.execute("PRAGMA foreign_keys = ON;")
+    return connection
+
+#setups the db helper function
+async def setup():
+    async with await connection() as conn:
+        await conn.execute("""
+        CREATE TABLE IF NOT EXISTS warns (
+            warn_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            server_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            reason TEXT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+
+        await conn.commit()
